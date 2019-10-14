4aebb77ac36c3830907f885daeaebb31912a1bd0
higorhms <higorhmscebrac@hotmail.com>
2019-10-14 16:37:32 -0300

    Adicionando o upload de imagens

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 import './database/index';
@@ -7,12 +8,17 @@ import './database/index';
 class App {
     constructor() {
         this.server = express();
+
         this.middlewares();
         this.routes();
     }
 
     middlewares() {
         this.server.use(express.json());
+        this.server.use(
+            '/files',
+            express.static(path.resolve(__dirname, '..', 'temp', 'uploads'))
+        );
     }
 
     routes() {
